6dd1473a85737195b78247c849bfb46db57c8938
devGiyosiddin <devgiyosiddin@gmail.com>
2024-04-05 14:29:06 +0500

    Add function auto detect prefer theme user's system part 2 FINAL

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,16 @@
 const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
-const body = window.document.body;
+const body = document.body;
 
 elSiteThemeToggler.addEventListener('click', function () {
-  document.body.classList.toggle("dark-mode");
-  elSiteThemeToggler.checked = document.body.classList.contains("dark-mode");
+  // dark-mode klassini yoqish va o'chirish
+  body.classList.toggle("dark-mode");
+  // localStorage ga qiymatni saqlash
+  localStorage.setItem('dark', body.classList.contains("dark-mode"));
 });
 
-elSiteThemeToggler.addEventListener('change', function() {
-  localStorage.setItem('dark', this.checked);
-  if (this.checked) {
-    body.classList.add('dark-mode');
-  } else {
-    body.classList.remove('dark-mode');
-  }
-});
-
+// Saqlangan holatni tekshirish va localStorage dan foydalanish
 if (localStorage.getItem('dark') === 'true') {
   body.classList.add('dark-mode');
-  elSiteThemeToggler.checked = true;
+} else {
+  body.classList.remove('dark-mode');
 }
